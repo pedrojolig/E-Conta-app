cc6139e5c0e135417f846092cfe3f953d62227ce
Pedro Lima <jpg.lima2011@gmail.com>
2021-05-20 14:37:15 -0300

    adicionar atualização no código, removendo     let cliente = null;

--- obter-dados.js
+++ obter-dados.js
@@ -1,6 +1,5 @@
 function obterDadosDoCliente(cpf, senha) {
     const clientes = JSON.parse(dados);
-    let cliente = null;
     for (let i = 0; i < clientes.length; i++) {
         if (clientes[i].cpf == cpf && clientes[i].senha == senha) {
             cliente = clientes[i];
