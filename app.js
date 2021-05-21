0c8bab65e671ac301955975ac4f4192c0fbb2d09
Pedro Lima <jpg.lima2011@gmail.com>
2021-05-20 22:34:13 -0300

    adicionar refatoração da exibição do extrato

--- app.js
+++ app.js
@@ -2,6 +2,3 @@ let cliente = null;
 
 document.addEventListener("DOMContentLoaded", verificarLogin);
 
-document.getElementById("linkExtrato").addEventListener("click", carregarExtrato);
-
-document.getElementById("linkInicio").addEventListener("click", carregarTelaPrincipal);
--- tela-principal.js
+++ tela-principal.js
@@ -1,8 +1,12 @@
 function carregarTelaPrincipal() {
     document.getElementById("conteudo").innerHTML = `
     <header>
-    <h1>Bem Vindo/a ${cliente.nome}</h1>
+        <h1>Bem Vindo/a ${cliente.nome}</h1>
     </header>
     <p>Seu Saldo é ${cliente.saldo}</p>
+    <button id="verExtrato">Ver Extrato</button>
+    <asid id="extrato">
+    </asid>
     `;
+    document.getElementById("verExtrato").addEventListener("click", carregarExtrato);
 }
